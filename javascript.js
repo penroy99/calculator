42e13ad57e523b741f913548a357940dc5ab6767
penroy99 <roy2.zichroni@gmail.com>
2023-05-05 21:34:49 +0300

    finish the code without decimal point function. it's not great logic but it is something

--- javascript.js
+++ javascript.js
@@ -12,28 +12,16 @@ function operate(operator, a, b) {
     return mathFunctions[operator](a, b)
 }
 
-/* numbers.forEach((number) => {
-    number.addEventListener('click', () => {
-        displayScreen.textContent += number.textContent;
-        firstNum += number.textContent;
-    })
-}); */
-
-
 let firstNum = null;
 let operator = null;
 let secondNum = null;
 
-
 const displayScreen = document.querySelector('#display');
-const numbers = document.querySelectorAll('#num');
-
 
 document.addEventListener('keydown', (e) => {
     const name = e.key;
-    console.log(name);
     if (name in mathFunctions && firstNum === null) {
-        console.log('No numbers were selected to operate on. Try again');
+        alert('No numbers were selected to operate on. Try again');
     } else if (name >= 0 && operator === null) {
         if (firstNum === null) {
             firstNum = 0;
@@ -41,11 +29,8 @@ document.addEventListener('keydown', (e) => {
         }
         displayScreen.textContent += name;
         firstNum += name;
-        console.log(firstNum)
-
     } else if (name in mathFunctions && firstNum != null) {
         operator = name;
-        console.log(operator)
     } else if (name >= 0) {
         if (secondNum === null) {
             secondNum = 0;
@@ -53,10 +38,61 @@ document.addEventListener('keydown', (e) => {
         }
         displayScreen.textContent += name;
         secondNum += name;
-        console.log(secondNum);
     } else if (name === '=' && (firstNum != null && secondNum != null)) {
         firstNum = operate(operator, firstNum, secondNum);
+        secondNum = null;
+        operator = null;
         displayScreen.textContent = firstNum;
     }
 }, false);
 
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let btn = button.textContent
+        if (btn in mathFunctions && firstNum === null) {
+            alert('No numbers were selected to operate on. Try again');
+        } else if (btn >= 0 && operator === null) {
+            if (firstNum === null) {
+                firstNum = 0;
+                displayScreen.textContent = '';
+            }
+            displayScreen.textContent += btn;
+            firstNum += btn;
+        } else if (btn in mathFunctions && firstNum != null) {
+            operator = btn;
+        } else if (btn >= 0) {
+            if (secondNum === null) {
+                secondNum = 0;
+                displayScreen.textContent = '';
+            }
+            displayScreen.textContent += btn;
+            secondNum += btn;
+        } else if (btn === '=' && (firstNum != null && secondNum != null)) {
+            firstNum = operate(operator, firstNum, secondNum);
+            displayScreen.textContent = firstNum;
+        }
+    })
+})
+
+const clearAll = document.querySelector('#ac');
+clearAll.addEventListener('click', () => {
+    displayScreen.textContent = '0';
+    firstNum = null;
+    secondNum = null;
+    operator = null;
+});
+
+const clear = document.querySelector('#c');
+clear.addEventListener('click', () => {
+        let output = displayScreen.textContent.toString();
+        output = output.slice(0, -1);
+        firstNum = Number(output);
+        displayScreen.textContent = Number(output);
+
+})
+
+const point = document.querySelector('#point');
+point.addEventListener('click', () => {
+    alert('Adding decimal point will only be available on the next version. We\'re sorry for the inconvenience.')
+})
